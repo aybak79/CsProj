c457406c299a77aea49ccc3b15283d8c50a0dc51
FolkDragoon~ <omarosman3122006@gmail.com>
2026-04-13 19:54:48 +0200

    added first three methods for Board class

--- DoorDashM1/src/game/engine/Board.java
+++ DoorDashM1/src/game/engine/Board.java
@@ -18,6 +18,26 @@ public class Board {
 		originalCards = readCards;
 		cards = new ArrayList<Card>();
 	}
+
+	private int[] indexToRowCol(int index) {
+		index = index % 100;
+		int row = Constants.BOARD_ROWS - 1 - (index / Constants.BOARD_ROWS);
+		int col = index % Constants.BOARD_COLS;
+		if(row % 2 == 1) {
+			col = Constants.BOARD_COLS - 1 - col;
+		}
+		return new int[] {row, col};
+	}
+
+	private Cell getCell(int index) {
+		int[] rowCol = indexToRowCol(index);
+		return boardCells[rowCol[0]][rowCol[1]];
+	}
+
+	private void setCell(int index, Cell cell) {
+		int[] rowCol = indexToRowCol(index);
+		boardCells[rowCol[0]][rowCol[1]] = cell;
+	}
 	
 	public Cell[][] getBoardCells() {
 		return boardCells;
