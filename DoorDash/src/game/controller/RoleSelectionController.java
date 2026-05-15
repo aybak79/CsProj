3453b99b4d3fb6bf2b6137567818bdceed6438b7
aybak79 <omaraymanyasser2006@gmail.com>
2026-05-15 16:07:32 +0300

    invalidcsv

--- DoorDash/src/game/controller/RoleSelectionController.java
+++ DoorDash/src/game/controller/RoleSelectionController.java
@@ -1,12 +1,16 @@
 package game.controller;
 
+import java.io.IOException;
+
 import game.engine.Game;
 import game.engine.Role;
+import game.engine.exceptions.InvalidCSVFormat;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -33,8 +37,18 @@ public class RoleSelectionController {
             Scene scene = new Scene(loader.load());
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(scene);
-        } catch (Exception e) {
-            e.printStackTrace();
+        }catch(InvalidCSVFormat e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Data Loading Error");
+            alert.setHeaderText("Invalid CSV Format");
+            alert.setContentText("A CSV file could not be loaded:\n\n" + e.getMessage());
+            alert.showAndWait();        
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("An error occurred");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
         }
     }
 }
