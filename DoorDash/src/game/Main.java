a8aa95af8f915399f942f13c6e11cdb10c580dfd
aybak79 <omaraymanyasser2006@gmail.com>
2026-05-18 15:37:52 +0300

    mm

--- DoorDash/src/game/Main.java
+++ DoorDash/src/game/Main.java
@@ -3,8 +3,6 @@ package game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,15 +11,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/game/view/views/StartView.fxml"));
         Scene scene = new Scene(loader.load());
-        String musicFile = getClass().getResource("/game/view/music/menu.mp3").toExternalForm();
-        Media sound = new Media(musicFile);
-        MediaPlayer mediaPlayer = new MediaPlayer(sound);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        mediaPlayer.play();
-        primaryStage.setTitle("Main Menu");
+        primaryStage.setTitle("Door Dash");
+        primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitHint("");
-        primaryStage.setScene(scene);
         primaryStage.show();
     }
     
