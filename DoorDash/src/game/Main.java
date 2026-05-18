5e5b3020ecb8f8d8e600120fec21182ff830d85a
FolkDragoon~ <omarosman3122006@gmail.com>
2026-05-18 16:40:59 +0300

    did

--- DoorDash/src/game/Main.java
+++ DoorDash/src/game/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
         primaryStage.setTitle("Main Menu");
         primaryStage.setFullScreen(true);
         primaryStage.setFullScreenExitHint("");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
     
